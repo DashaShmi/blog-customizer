6101b4909be191aad5af3b2e0025a48751fb93d5
Daria Dmitrienko <ishmizh@gmail.com>
2024-10-08 15:12:13 +0400

    начала писать сайд бар

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,22 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { useState } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+
+	const [sidebarIsOpen, setIsSidebarOpen] = useState(false);
+
+	const toggleSidebar = () => {
+		// Вызов функции для изменения состояния
+		setIsSidebarOpen(!sidebarIsOpen);
+	}
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={toggleSidebar} />
+			<aside className={`${styles.container} ${sidebarIsOpen ? styles.container_open : ''}`}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />

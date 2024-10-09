1f64c6dfd732a26fd9ca1cd4a5ec0ce37d5038da
Daria Dmitrienko <ishmizh@gmail.com>
2024-10-09 21:04:43 +0400

    делаю

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,21 +3,81 @@ import { Button } from 'components/button';
 import { useState } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group'
+import { fontFamilyOptions, fontColors, backgroundColors, contentWidthArr, fontSizeOptions, defaultArticleState } from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
 
-	const [sidebarIsOpen, setIsSidebarOpen] = useState(false);
+interface propsArticleParamsForm {
+	onChange?: () => void;
+}
+
+export const ArticleParamsForm = ({ onChange }: propsArticleParamsForm) => {
+
+	const [sidebarIsOpen, setIsSidebarOpen] = useState(true);
 
 	const toggleSidebar = () => {
 		// Вызов функции для изменения состояния
 		setIsSidebarOpen(!sidebarIsOpen);
 	}
+	const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log('вонь формы, чуешь?');
+		if (onChange) {
+			onChange()
+		}
+	}
+
+	const [selectedFont, setSelectedFont] = useState(defaultArticleState.fontFamilyOption);
+	const [selectedFontSize, setSelectedFontSize] = useState(defaultArticleState.fontSizeOption);
+	const [selectedFontColor, setSelectedColor] = useState(defaultArticleState.fontColor);
+	const [selectedBackgroundColor, setSelectedBackgroundColor] = useState(defaultArticleState.backgroundColor);
+	const [selectedWidthContent, setSelectedWidthContent] = useState(defaultArticleState.contentWidth);
 
 	return (
 		<>
 			<ArrowButton onClick={toggleSidebar} />
 			<aside className={`${styles.container} ${sidebarIsOpen ? styles.container_open : ''}`}>
-				<form className={styles.form}>
+
+				<form className={styles.form} onSubmit={submitHandler}>
+
+					<Select
+						selected={selectedFont}
+						onChange={setSelectedFont}
+						options={fontFamilyOptions}
+						title='шрифт'
+					/>
+
+					<RadioGroup
+						selected={selectedFontSize}
+						name='radio'
+						onChange={setSelectedFontSize}
+						options={fontSizeOptions}
+						title='размер шрифта'
+					/>
+
+					<Select
+						selected={selectedFontColor}
+						onChange={setSelectedColor}
+						options={fontColors}
+						title='цвет шрифта'
+					/>
+
+
+					<Select
+						selected={selectedBackgroundColor}
+						onChange={setSelectedBackgroundColor}
+						options={backgroundColors}
+						title='цвет фона'
+					/>
+
+					<Select
+						selected={selectedWidthContent}
+						onChange={setSelectedWidthContent}
+						options={contentWidthArr}
+						title='ширина контента'
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
